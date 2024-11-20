d4da8d0392ca25d96603c313220cda045c2e0404
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2024-11-20 02:05:03 -0500

    a

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const apiEntry = "https://restcountries.com/v3.1/all";
+console.log(fetch(apiEntry));
+
+fetch(apiEntry)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+async function fetchData(apiEntry) {
+  try {
+    const response = await fetch(apiEntry);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+fetchData(apiEntry);
