6a1099ab9ce231fd094fcc83e0767bde8a91019a
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2024-11-26 01:40:13 -0500

    ea

--- app/main.js
+++ app/main.js
@@ -24,14 +24,18 @@ function createCountryCard(countries) {
     container.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="country-card">
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.svg}" alt="Flag of ${country.name.common}" width="100">
-        <p><strong>Region:</strong> ${country.region}</p>
-        <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
-        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-        <p><strong>Languages:</strong> ${Object.values(country.languages || {}).join(", ")}</p>
-      </div>
+        <div class="box rounded-2xl bg-neutral border-secondary border mx-auto my-4 w-5/12 md:w-5/12 h-96 p-4 min-w-[12em] text-center">
+          <h2 class="text-lg font-bold text-base-100">${country.name.common}</h2>
+          <div class="content flex items-start gap-4 mt-4">
+            <img src="${country.flags.svg}" alt="Flag of ${country.name.common}" class="h-60 w-[17em] border-r-2 border-base-100 pr-4">
+            <div class="details text-left text-base-100 space-y-2">
+              <p><strong>Region:</strong> ${country.region}</p>
+              <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+              <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+              <p><strong>Languages:</strong> ${Object.values(country.languages || {}).join(", ")}</p>
+            </div>
+          </div>
+        </div>
       `
     );
   });
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["retro"],
+  },
   theme: {
     extend: {},
   },
